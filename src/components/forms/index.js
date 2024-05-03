68b2430287dcf3c716d68eebecabec40add52cc9
Vanessa Fuangi <vanessafuangi@gmail.com>
2024-05-03 16:04:55 +0100

    Removed the sidebar on the authentication forms

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,5 +1,4 @@
 export { default as Signup } from "./Signup";
 export { default as Login } from "./Login";
 export { default as ForgotPassword } from "./ForgotPassword";
-export { default as FormSidebar } from "./FormSidebar";
 export { default as FormButton } from "./FormButton";
--- src/screens/Authentication.jsx
+++ src/screens/Authentication.jsx
@@ -1,13 +1,9 @@
-// import { Loader } from "../components/Layout";
-// import { Outlet } from "react-router-dom";
 import AuthLayout from "../components/Layout/AuthLayout";
-import { FormSidebar } from "../components/forms";
 
 function Authentication() {
   return (
     <div className="authentication">
       <AuthLayout />
-      <FormSidebar />
     </div>
   );
 }
