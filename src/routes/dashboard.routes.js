67d6d9e76249d9f5c5be8a93c88c0ce45a9496fb
Fuangi <108869150+Fuangi@users.noreply.github.com>
2024-05-10 13:36:37 +0100

    Refactored the routes abit

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -8,13 +8,12 @@ import Dashboard from "../components/dashboard/Dashboard";
 function DashRoute() {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />}>
-        <Route path="setting" element={<Settings />} />
-        <Route path="trash" element={<Trash />} />
-        <Route path="files" element={<Files />} />
-        <Route path="me" element={<p>This is for your information</p>} />
-        <Route path="notifications" element={<Notifications />} />
-      </Route>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="setting" element={<Settings />} />
+      <Route path="trash" element={<Trash />} />
+      <Route path="files" element={<Files />} />
+      <Route path="me" element={<p>This is for your information</p>} />
+      <Route path="notifications" element={<Notifications />} />
     </Routes>
   );
 }
