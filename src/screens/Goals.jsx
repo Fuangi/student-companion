26cd35cad8371fcf94d729741968ab23a03f8609
Fuangi <108869150+Fuangi@users.noreply.github.com>
2024-05-13 11:55:56 +0100

    Added sample content to the goals layout

--- src/screens/Goals.jsx
+++ src/screens/Goals.jsx
@@ -1,6 +1,6 @@
 import { FaList } from "react-icons/fa6";
 import { DashLayout } from "../components/Layout";
-// import CreateGoal from "../components/goals/CreateGoal";
+import MyCalendar from "../components/planner/Calendar";
 
 function Goals() {
   return (
@@ -10,21 +10,51 @@ function Goals() {
         <div className="my-goals-header">
           <h2>My Goals...</h2>
         </div>
-        <h3>%Something inspirational here</h3>
-
+        <h3 className="goal-inspiration">%Something inspirational here</h3>
         <div className="my-goals-body">
           <h4>Happiness is freeee...</h4>
           <div className="goals-container">
             <div className="goals">
-              {}
-              <div>
-                <h5>Daily Goals</h5>
-                <p>
-                  <FaList /> Your Daily Goals
-                </p>
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+              <Goal />
+            </div>
+            <div className="affirmations">
+              <div className="datetime">
+                <MyCalendar
+                  defaultView="month"
+                  views={["month", "day", "agenda"]}
+                />
+              </div>
+              <div className="affirm">
+                <h4>My affirmations</h4>
+                <ol>
+                  <li>I affirm that</li>
+                  <li>I affirm that</li>
+                  <li>I affirm that</li>
+                  <li>I affirm that</li>
+                  <li>I affirm that</li>
+                </ol>
               </div>
             </div>
-            <div className="affirmations">Some other things here</div>
           </div>
         </div>
       </div>
@@ -33,3 +63,14 @@ function Goals() {
 }
 
 export default Goals;
+
+export function Goal() {
+  return (
+    <div>
+      <h5>Daily Goals</h5>
+      <p>
+        <FaList className="goal-icon" /> Your Daily Goals
+      </p>
+    </div>
+  );
+}
