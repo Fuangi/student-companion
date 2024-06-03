d1760d712addba62bf4dafa46d0a9ffa59f57573
Fuangi <108869150+Fuangi@users.noreply.github.com>
2024-06-03 12:32:31 +0100

    Added a responsive menu to the mobile screen

--- src/components/Layout/DashLayout.jsx
+++ src/components/Layout/DashLayout.jsx
@@ -1,14 +1,11 @@
 // import { Outlet } from "react-router-dom";
 import { DashSideNav } from "../dashboard";
-import { MdClose, MdMenu } from "react-icons/md";
 
 function DashLayout({ children }) {
   return (
     <div className="dash-layout">
       {/* <DashTopNav /> */}
       <div className="mid-layout">
-        <MdMenu className="small-screen-menu" />
-        <MdClose className="small-screen-menu close" />
         <DashSideNav />
         <main className="layout-children">{children}</main>
       </div>
--- src/components/dashboard/DashSideNav.jsx
+++ src/components/dashboard/DashSideNav.jsx
@@ -8,55 +8,77 @@ import {
   FaPeopleGroup,
   FaTrash,
 } from "react-icons/fa6";
-import { MdSettings } from "react-icons/md";
+import { MdClose, MdMenu, MdSettings } from "react-icons/md";
+import { useState } from "react";
 
 function DashSideNav() {
+  const [showMenu, setShowMenu] = useState(true);
   return (
-    <div className="dashboard-side-nav">
-      <ul className="dash-side">
-        <li>
-          <NavLink to="/dashboard" end>
-            Home <FaHouse className="dash-side-icon" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/plans">
-            Plans <FaClipboardList className="dash-side-icon" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/goals">
-            Goals <FaList className="dash-side-icon" />
-          </NavLink>
-        </li>
+    <>
+      <div
+        className="dashboard-side-nav"
+        style={{
+          width: `${window.innerWidth <= 700 && showMenu ? "50%" : ""}`,
+        }}
+      >
+        {showMenu ? (
+          <MdClose
+            className="side-menu"
+            onClick={() => {
+              setShowMenu(false);
+            }}
+            style={{ color: "white" }}
+          />
+        ) : (
+          <MdMenu className="side-menu" onClick={() => setShowMenu(true)} />
+        )}
+        <>
+          <ul className="dash-side">
+            <li>
+              <NavLink to="/dashboard" end>
+                Home <FaHouse className="dash-side-icon" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/plans">
+                Plans <FaClipboardList className="dash-side-icon" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/goals">
+                Goals <FaList className="dash-side-icon" />
+              </NavLink>
+            </li>
 
-        <li>
-          <NavLink to="/forums">
-            Forums <FaPeopleGroup className="dash-side-icon" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/resources">
-            Res <FaNewspaper className="dash-side-icon" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/files">
-            Files <FaFolder className="dash-side-icon" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/trash">
-            Trash <FaTrash className="dash-side-icon" />
-          </NavLink>
-        </li>
-      </ul>
-      <div className="settings">
-        <NavLink to="/setting">
-          Settings <MdSettings className="dash-side-icon" />
-        </NavLink>
+            <li>
+              <NavLink to="/forums">
+                Forums <FaPeopleGroup className="dash-side-icon" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/resources">
+                Res <FaNewspaper className="dash-side-icon" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/files">
+                Files <FaFolder className="dash-side-icon" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/trash">
+                Trash <FaTrash className="dash-side-icon" />
+              </NavLink>
+            </li>
+          </ul>
+          <div className="settings" style={{ marginBottom: "2rem" }}>
+            <NavLink to="/setting">
+              Settings <MdSettings className="dash-side-icon" />
+            </NavLink>
+          </div>
+        </>
       </div>
-    </div>
+    </>
   );
 }
 
